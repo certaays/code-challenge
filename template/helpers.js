30ae4daeee31b8592ac5b43a1a3ef39e1f8b68f1
Jansen-c <jansen08r@gmail.com>
2021-10-08 15:53:16 +0700

    update

--- template/helpers.js
+++ template/helpers.js
@@ -1,39 +1,58 @@
 export const getPosts = async () => {
   // EDIT HERE
-};
-
-export const getPost = async (post_id) => {
-  // EDIT HERE
-};
-
-export const getPostComments = async (post_id) => {
-  // EDIT HERE
-};
-
-export const getAuthor = async (user_id) => {
-  // EDIT HERE
-};
-
-export const getPostsByAuthor = async (author_id) => {
-  // EDIT HERE
-};
-
-export const getRandomPic = async () => {
   try {
-    const image = await fetch('https://source.unsplash.com/random/720x480');
-    return image.url;
-  } catch (error) {
-    console.log('getRandomPic', error);
-    throw error;
-  }
-};
+   const posts = await fetch('https://jsonplaceholder.typicode.com/posts')
+   .then(response => response.json())
+   return posts;
 
-export const getRandomProfile = async () => {
-  try {
-    const image = await fetch('https://source.unsplash.com/480x480/?profile');
-    return image.url;
-  } catch (error) {
-    console.log('getRandomProfile', error);
-    throw error;
-  }
-};
+ } catch (error) {
+   console.log(error);
+   throw error;
+ }
+ };
+ 
+ export const getPost = async (post_id) => {
+   // EDIT HERE
+   // try{
+   //   const response = await fetch{
+   //     `https://jsonplaceholder.typicode.com/posts${post_id}`,
+   //     {
+ 
+   //     }
+   //   }
+ 
+   // }
+ };
+ 
+ export const getPostComments = async (post_id) => {
+   // EDIT HERE
+ 
+ };
+ 
+ export const getAuthor = async (user_id) => {
+   // EDIT HERE
+ };
+ 
+ export const getPostsByAuthor = async (author_id) => {
+   // EDIT HERE
+ };
+ 
+ export const getRandomPic = async () => {
+   try {
+     const image = await fetch('https://source.unsplash.com/random/720x480');
+     return image.url;
+   } catch (error) {
+     console.log('getRandomPic', error);
+     throw error;
+   }
+ };
+ 
+ export const getRandomProfile = async () => {
+   try {
+     const image = await fetch('https://source.unsplash.com/480x480/?profile');
+     return image.url;
+   } catch (error) {
+     console.log('getRandomProfile', error);
+     throw error;
+   }
+ };
